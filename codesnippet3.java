30f9081d9aef0cc9aa19c1a98c358d177955324c
HaVanVu1708 <62437220+HaVanVu1708@users.noreply.github.com>
2020-03-21 00:10:03 +0700

    Initial commit

---
+++ codesnippet3.java
@@ -0,0 +1,16 @@
+
+package Sesson2;
+
+public class codesnippet3 {
+    public static void main(String[] args) {
+        /*
+         *    This code performs mathematical
+         *     operation of adding two numbers.
+         */
+        int a = 20;
+        int b = 30;
+        int c;
+        c = a + b;
+        System.out.println(c);
+    }
+}
